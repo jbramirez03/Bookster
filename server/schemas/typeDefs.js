8662a650262fa0e7f4b27e8333200bdabdaf07c2
temptado <jason1287712@gmail.com>
2021-10-28 19:19:19 -0400

    added type defs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Book {
-        bookId: String!
-        authors: [String!]
-        description: String!
-        image: String!
-        link: String!
-        title: String!
-    }
 
-    
-    type User {
-        _id: ID
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-      }
-    
-    type Query {
-        users: [User]
-        books: [Book]
-    }
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+}
+  
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+  
+  type Auth {
+    token: ID!
+    user: User
+}
+  
+  type Query {
+    me: User
+}
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+}
+
+  input savedBook {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+}
 
 
 `;
